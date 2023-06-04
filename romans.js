b00165217be0f5eaa2d84959185d7450543771e4
Guillermo Pérez <61142995+Wibaz@users.noreply.github.com>
2023-06-04 17:55:40 +0200

    Undone the gtag js function

--- romans.js
+++ romans.js
@@ -32,11 +32,7 @@ function init() {
     var convertion = modeCheckbox.checked ? convertIntegerToRoman(inputValue) : convertRomanToInteger(inputValue);
     if (convertion.result) {
       outputArea.innerHTML = convertion.value;
-      
-      // Add the analytics
-      gtag('event', 'conversion_done', {
-        'conversion_type':  modeCheckbox.checked ? 'integer_to_roman' : 'roman_to_integer'
-      });
+  
     } else {
       alert(convertion.message);
     }
